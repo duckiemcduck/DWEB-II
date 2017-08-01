b708621a3a01418d59172f733e867bfd76605c7b
Aluno <lucascoliado@hotmail.com>
2017-08-01 16:16:09 -0300

    Adicionado [Lista 1]Exercicio 11

---
+++ lista/1/11/controller/calculo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Fundamentos da Linguagem PHP </title>
+        <link rel="stylesheet" href="../../stylesheets/global.css">        
+		<link rel="stylesheet" href="../../stylesheets/9.css">        
+    </head>
+    <body>
+        <h1> Exercício 9 - Formulários e Matemática com PHP </h1> 
+        <div id="container-conteudo" class="resultado">
+                <?php
+                    $nota1 = $_POST["nota1"];
+                    $nota2 = $_POST["nota2"];
+                    $peso1 = $_POST["peso1"];
+                    $peso2 = $_POST["peso1"];
+                    $nome = $_POST["nome"];
+                    echo "Nota de $nome:";
+                    echo "<p>",($nota1*$peso1+$nota2*$peso2)/($peso1+$peso2),"</p>";
+                ?>
+        </div>
+</html>
+
---
+++ lista/1/11/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Fundamentos da Linguagem PHP </title>
+        <link rel="stylesheet" href="../stylesheets/global.css">        
+		<link rel="stylesheet" href="../stylesheets/9.css">        
+    </head>
+    <body>
+        <h1> Exercício 11 - Média Aritmética de Formulário PHP</h1>     
+        <div id="container-conteudo">
+            <form action="./controller/calculo.php" method="post">
+                <fieldset>
+                    <legend> Cálculo numérico com valores quaisquer </legend>
+                    <div id="tabela">
+                        <div id=labels>
+                            <label class="alinhar"> Forneça a primeira nota</label>
+                            <label class="alinhar"> Forneça a segunda nota</label>
+                            <label class="alinhar"> Forneça o nome do aluno:</label>
+                        </div>
+                        <div id=inputs>
+                            <input type="number" step="0.1" min="0" max="10" name="nota1">
+                            <input type="number" step="0.1" min="0" max="10" name="nota2">
+                            <input type="string" name="nome">
+                        </div>
+                         <div id=labels>
+                            <label class="alinhar"> Peso 1:</label>
+                            <label class="alinhar"> Peso 2:</label>
+                        </div>
+                        <div id=inputs>
+                            <input type="number" name="peso1">
+                            <input type="number" name="peso2">
+                        </div>
+                    </div>
+                </fieldset>
+                <button id="botao-enviar" type="submit" name="enviar"> Processar com PHP </button>
+            </form>
+            
+        </div>
+    </body>
+</html>
+
